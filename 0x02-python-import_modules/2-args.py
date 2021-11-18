e93ff6d5f78839e074e15c5caa94828f2904b02d
Temitope Olamolu <olamolutemitope@gmail.com>
2021-11-18 11:43:29 +0100

    fixed formating strings

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     elif count == 1:
         print("1 arguments:")
     else:
-        print(f"{count} arguments:")
+        print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
