a32a7ca4158283da3d216cd6a681c549905be75f
Temitope Olamolu <olamolutemitope@gmail.com>
2021-11-24 13:28:26 +0100

    map

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        key *= 2
-        return({k: a_dictionary[key]})
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
