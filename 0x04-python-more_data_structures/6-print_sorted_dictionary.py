d3a29bb609fb2ac83d17cbb425d828d7f939637b
Temitope Olamolu <olamolutemitope@gmail.com>
2021-11-24 13:10:40 +0100

    fix missing bracket)

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for sort in sorted(a_dictionary):
-        print("{}: {}".format(sort, a_dictionary[sort])
-              
+        print("{}: {}".format(sort, a_dictionary[sort]))
